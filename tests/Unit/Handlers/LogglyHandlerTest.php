b7b207f9fa9da146449a6c6e83d7e88d06ecdf36
Kos <koshuang@gmail.com>
2021-01-06 11:46:21 +0800

    feat(handler): [LogglyHandler] added to support setting tags on constructor

---
+++ tests/Unit/Handlers/LogglyHandlerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Onramplab\LaravelLogEnhancement\Tests\Unit\Handlers;
+
+use Onramplab\LaravelLogEnhancement\Tests\TestCase;
+use Mockery;
+use Onramplab\LaravelLogEnhancement\Handlers\LogglyHandler;
+
+class LogglyHandlerTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function constructor_should_support_tags()
+    {
+      $this->handler = Mockery::mock(LogglyHandler::class)->makePartial();
+
+      $this->handler->shouldReceive('setTag')->with(['tag1', 'tag2'])->once();
+
+      $this->handler->__construct('test', 'tag1,tag2');
+    }
+}
