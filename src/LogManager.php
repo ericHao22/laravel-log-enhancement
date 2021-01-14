0d4612d6161267c5a7fff432c89e0f869e982913
Kos <koshuang@gmail.com>
2021-01-14 18:50:48 +0800

    feat: [LogManager] added to extend Laravel's LogManager and replace Logger to our custom Logger

---
+++ src/LogManager.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Onramplab\LaravelLogEnhancement;
+
+use Illuminate\Log\LogManager as IlluminateLoggerManager;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger as Monolog;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+/**
+ * Replace with our custom Logger
+ */
+class LogManager extends IlluminateLoggerManager
+{
+    /**
+     * Create an emergency log handler to avoid white screens of death.
+     *
+     * @return \Psr\Log\LoggerInterface
+     */
+    protected function createEmergencyLogger()
+    {
+        $config = $this->configurationFor('emergency');
+
+        $handler = new StreamHandler(
+            $config['path'] ?? $this->app->storagePath().'/logs/laravel.log',
+            $this->level(['level' => 'debug'])
+        );
+
+        return new Logger(
+            new Monolog('laravel', $this->prepareHandlers([$handler])),
+            $this->app['events']
+        );
+    }
+
+    /**
+     * Create a new, on-demand aggregate logger instance.
+     *
+     * @param  array  $channels
+     * @param  string|null  $channel
+     * @return \Psr\Log\LoggerInterface
+     */
+    public function stack(array $channels, $channel = null)
+    {
+        return new Logger(
+            $this->createStackDriver(compact('channels', 'channel')),
+            $this->app['events']
+        );
+    }
+
+    /**
+     * Attempt to get the log from the local cache.
+     *
+     * @param  string  $name
+     * @return \Psr\Log\LoggerInterface
+     */
+    protected function get($name)
+    {
+        try {
+            return $this->channels[$name] ?? with($this->resolve($name), function ($logger) use ($name) {
+                return $this->channels[$name] = $this->tap($name, new Logger($logger, $this->app['events']));
+            });
+        } catch (Throwable $e) {
+            return tap($this->createEmergencyLogger(), function ($logger) use ($e) {
+                $logger->emergency('Unable to create configured logger. Using emergency logger.', [
+                    'exception' => $e,
+                ]);
+            });
+        }
+    }
+}
